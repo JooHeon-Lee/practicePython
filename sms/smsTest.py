1ebddedaf1a3d205fa04fc06193f9fd6794519d4
JooHeon-Lee <58197251+JooHeon-Lee@users.noreply.github.com>
2021-07-26 23:29:31 +0900

    Create smsTest.py
    SMS API테스트

---
+++ sms/smsTest.py
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = 'SID입력'
+auth_token = '토큰'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+    .create(
+         from_='+18575765221', #발신번호
+         body='TEST', #내용
+         to='+821031636589'#받는사람 번호
+     )
+
+print(message.sid)
